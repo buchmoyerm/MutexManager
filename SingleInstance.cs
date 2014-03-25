69703ee0c4e29e3169d3ca0e4c9335faa1653be2
Mark Buchmoyer <buchmoyerm@gmail.com>
2014-03-25 17:42:48 -0400

    Change to global instance

--- SingleInstance.cs
+++ SingleInstance.cs
@@ -32,11 +32,11 @@ namespace MutexManager
         static public bool Start()
         {
             bool onlyInstance = false;
-            string mutexName = String.Format("Local\\{0}", ProgramInfo.AssemblyGuid);
+            //string mutexName = String.Format("Local\\{0}", ProgramInfo.AssemblyGuid);
 
             // if you want your app to be limited to a single instance
             // across ALL SESSIONS (multiple users & terminal services), then use the following line instead:
-            // string mutexName = String.Format("Global\\{0}", ProgramInfo.AssemblyGuid);
+            string mutexName = String.Format("Global\\{0}", ProgramInfo.AssemblyGuid);
 
             mutex = new Mutex(true, mutexName, out onlyInstance);
             return onlyInstance;
